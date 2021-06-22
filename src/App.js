ad54ccbd8bdbedbd746a85b23aa9c3a3594d776e
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-06-22 20:00:44 +0530

    Create Card Component and Routes for pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Pages/Home";
+import Services from "./Components/Pages/Services.Js";
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home}></Route>
+          <Route path='/services' component={Services}></Route>
         </Switch>
       </Router>
+      {/* <img src='/images/img-1.jpg' alt='nothing'></img> */}
     </>
   );
 }
