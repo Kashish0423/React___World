7f5f439670ed74ef4dee0d63b1abb4a0556b24d5
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-06-23 16:45:02 +0530

    Functions to Update and Remove Included

--- src/Components/TodoApp/TodoList.js
+++ src/Components/TodoApp/TodoList.js
@@ -16,6 +16,23 @@ const TodoList = () => {
     setTodos(newtodos);
   };
 
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      // regex used for invalid inputs
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+
   const completeTodo = (id) => {
     let updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
@@ -30,7 +47,12 @@ const TodoList = () => {
     <div>
       <h1>What's the Plan for Today</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 };
