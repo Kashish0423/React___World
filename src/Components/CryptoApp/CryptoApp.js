c107e6c3f6a62df711d67360e57d6b69970d28d2
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-06-25 18:27:48 +0530

    Basic Crypto Setup Added

---
+++ src/Components/CryptoApp/CryptoApp.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false
+
+const CryptoApp = () => {
+  return <>Hello World</>;
+};
+
+export default CryptoApp;
--- src/Components/Pages/Products.js
+++ src/Components/Pages/Products.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "../../App.css";
 import Footer from "../Footer";
+import CryptoApp from "../CryptoApp/CryptoApp";
 
 const Products = () => {
-  const mystyle = {
-    backgroundImage: `url("/images/img-5.jpg")`,
-  };
+  // const mystyle = {
+  //   backgroundImage: `url("/images/img-5.jpg")`,
+  // };
 
   return (
     <>
-      <h1 className='products' style={mystyle}>
+      {/* <h1 className='products' style={mystyle}>
         {" "}
-        PRODUCTS
-      </h1>
+        Products
+      </h1> */}
+
+      <CryptoApp />
       <Footer />
     </>
   );
